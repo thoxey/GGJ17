3dea3ff978ef6b1dc617122f56ab217ea5f4504c
Tom Hoxey <tom.hoxey@gmail.com>
2017-01-21 02:29:45 +0000

    End of Day One
    Going to get some sleep

---
+++ Enemies.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemies : MonoBehaviour {
+
+	[HideInInspector] public List<GameObject> enemyPool;
+	public GameObject enemyStorer;
+	public GameObject enemyPrefab;
+	public GameObject player;
+
+	int maxEnemies = 30; //Max Enemies in the pool
+	int enemiesInWave = 0; //Enemies Spawned In a wave
+	int wavesize = 3; //Max Enemies to spawn before waiting
+	int spawnrate = 3; //Seconds before new spawn
+	int timer = 0;
+	int oldTimer = 0;
+	bool oneAtATime;
+	int speed = 3;
+	void generateEnemy(GameObject _enemy, int _side)
+	{
+		GameObject retenemy = (GameObject)Instantiate(_enemy);
+		retenemy.transform.parent = enemyStorer.transform;
+		if(_side%2 != 0)
+			retenemy.transform.position = new Vector3(Random.Range(-7.0f, 0.0f), 7, 0);
+		else
+			retenemy.transform.position = new Vector3(Random.Range(0.0f, 7.0f), 7, 0);
+		retenemy.SetActive (false);
+		enemyPool.Add(retenemy);
+	}
+	void generatePool()
+	{
+		Debug.Log ("Generating Pool");
+		for (int i = 0; i < maxEnemies; i++)
+		{
+			generateEnemy (enemyPrefab, i);
+		}
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		generatePool ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float step = speed * Time.deltaTime;
+		//Once a second
+		if (oldTimer != (int)Time.realtimeSinceStartup) 
+		{
+			oldTimer = (int)Time.realtimeSinceStartup;
+			Debug.Log (player.GetComponent<PlayerController> ().enemiesKilled);
+			oneAtATime = true;
+			timer++;
+		}
+		//New Wave
+		if (player.GetComponent<PlayerController> ().enemiesKilled == wavesize) 
+		{
+			Debug.Log ("End of Wave");
+			timer = 1;
+			wavesize ++;
+			speed++;
+			enemiesInWave = 0;
+			player.GetComponent<PlayerController> ().enemiesKilled = 0;
+		}
+		//Spawn Enemy
+		if(timer % spawnrate == 0 && enemiesInWave < wavesize)
+		{
+			if (oneAtATime) 
+			{
+				enemyPool [enemiesInWave].SetActive (true);
+				enemiesInWave++;
+				oneAtATime = false;
+			}
+		}
+
+		foreach (GameObject enemy in enemyPool) 
+		{
+			if (enemy.transform.position.x > player.transform.position.x) {
+				Vector3 target = new Vector3 (player.transform.position.x + 2, player.transform.position.y, player.transform.position.z);
+				enemy.transform.position = Vector3.MoveTowards (enemy.transform.position, target, step);
+			} 
+			else 
+			{
+				Vector3 target = new Vector3 (player.transform.position.x - 2, player.transform.position.y, player.transform.position.z);
+				enemy.transform.position = Vector3.MoveTowards (enemy.transform.position, target, step);
+			}
+		}
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+//Based off of boiler plate from Unity
+public class PlayerController : MonoBehaviour 
+{
+	#region Gloabls
+	[HideInInspector] public bool facingRight = true;
+	[HideInInspector] public bool jump = false;
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public float jumpForce = 1f;
+
+	public Collider2D ground;
+	public Collider2D leftSide;
+	public Collider2D rightSide;
+
+	public int enemiesKilled = 0;
+
+	bool doubleJump = true;
+	public Transform groundCheck;
+	float downV;
+	float olddownV;
+
+	bool grounded = false;
+	bool sided = false;
+	private Rigidbody2D rb2d;
+
+	GameObject[] platforms;
+
+	const int LEFT = 0;
+	const int RIGHT = 1;
+	const int sideBuffer = 1;
+
+	#endregion
+
+	void useWeapon(int _side, int _range)
+	{
+		int lor;
+		int buffer = sideBuffer;
+		_range = 2;
+		if (_side == RIGHT) 
+		{
+			Debug.Log("Shot Right");
+			lor = _range;
+		} 
+		else
+		{
+			Debug.Log("Shot Left");
+			buffer *= -1;
+			lor = -1*_range;
+		}
+		Vector3 origin = new Vector3(transform.position.x+buffer, transform.position.y, transform.position.z);
+		Vector3 dir = new Vector3(transform.position.x+lor, transform.position.y, transform.position.z);
+		Debug.DrawLine (origin, dir, Color.white, 3f, false);
+		RaycastHit2D hit = Physics2D.Raycast (origin, dir, 20);
+		if(hit.collider != null && hit.collider.tag == "Enemy")
+		{
+			enemiesKilled++;
+			hit.collider.gameObject.SetActive(false);
+		}
+	}
+
+
+
+	// Use this for initialization
+	void Awake () 
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		platforms = GameObject.FindGameObjectsWithTag ("Floor");
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		grounded = false;
+		sided = true;
+		foreach(GameObject platform in platforms)
+		{
+			if(rb2d.IsTouching(platform.GetComponent<Collider2D>()))
+			{
+				grounded = true;
+				doubleJump = true;
+			}
+			if(rb2d.IsTouching(leftSide) || rb2d.IsTouching(rightSide))
+			{
+				sided = false;
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow) && (grounded || doubleJump))
+		{
+			if(doubleJump)
+			{
+				doubleJump = false;
+			}
+			jump = true;
+		}
+		if (Input.GetKeyDown (KeyCode.Z)) 
+		{
+			useWeapon(LEFT, 10);
+		}
+		if (Input.GetKeyDown (KeyCode.X)) 
+		{
+			useWeapon(RIGHT, 10);
+		}
+
+
+
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
+
+		if ((h * rb2d.velocity.x < maxSpeed) && sided)
+			rb2d.AddForce(Vector2.right * h * moveForce);
+
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
+			rb2d.velocity = new Vector2(Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+		if (h > 0 && !facingRight)
+			Flip ();
+		else if (h < 0 && facingRight)
+			Flip ();
+
+		if (jump)
+		{
+			rb2d.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
+	}
+
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
